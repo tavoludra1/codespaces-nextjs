ddec78dcabe47ee00e45dd59da1eb0f59f5e9e12
GustavoP <42284294+tavoludra1@users.noreply.github.com>
2023-02-27 17:21:23 +0000

    prueba next

--- pages/index.js
+++ pages/index.js
@@ -28,9 +28,9 @@ function Home() {
 
   return (
     <main className={styles.main}>
-      <h1>Fast Refresh Demo</h1>
+      <h1>Fast Refresh Demo123</h1>
       <p>
-        Fast Refresh is a Next.js feature that gives you instantaneous feedback
+        The Fast Refresh is a Next.js feature that gives you instantaneous feedback
         on edits made to your React components, without ever losing component
         state.
       </p>
